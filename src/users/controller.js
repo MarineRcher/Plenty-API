beb7ec162e3ee8f57312e6110eedff00e5ed7adb
MarineRcher <106527470+MarineRcher@users.noreply.github.com>
2023-02-17 12:16:20 +0100

    add rm users

--- src/users/controller.js
+++ src/users/controller.js
@@ -21,7 +21,7 @@ if (results.rows.length) {
 		// add users to db
 		pool.query(queries.addUsers, [username, password], (error, results) => {
 				if (error) throw error;
-				res.status(201).send("Users créé avec succés !");
+				res.status(201).send("Users créé avec succès !");
 				
 		});
 	});
@@ -35,14 +35,17 @@ const getUsersById = (req, res) => {
 	});
 };
 
-const removeUsers = (res, req) => {
-	const id = parseInt(req,params.id); 
-	pool.query(queries.removeUser, [id], (error,results) => {
-		const noUserFound = !require.rows.length;
+const removeUsers = (req, res) => {
+	const id = parseInt(req.params.id); 
+	pool.query(queries.removeUsers,[id], (error, results) => {
+		const noUserFound = !results.rows.length;
 		if (noUserFound) {
 			res.send("L'User n'existe pas dans la base de donnée");
 		}
-		
+		pool.query(queries.removeUsers, [id], (error, results) => {
+			if (error) throw error;
+				res.status(200).send("User Supprimé avec succès");
+		});
 	});
 };
 
--- src/users/queries.js
+++ src/users/queries.js
@@ -2,11 +2,12 @@ const getUsers = "SELECT * FROM users";
 const getUsersById = "SELECT * FROM users WHERE id = $1";
 const checkUsernameExists = "SELECT s FROM users s WHERE s.username = $1"; 
 const addUsers = "INSERT INTO users (username, password) VALUES ($1, $2)";
-const = ""
+const removeUsers = "DELETE FROM users WHERE id = $1";
 
 module.exports = {
     getUsers,
     checkUsernameExists,
     getUsersById,
     addUsers,
+    removeUsers,
 };
