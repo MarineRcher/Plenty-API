ab53b20edc2cae0d15a0b9258968ebd1b38f8f0f
Kazeynma <kagnana.ith@ecole-isitech.fr>
2023-02-28 19:25:03 +0100

    retification bug

--- src/products/queries.js
+++ src/products/queries.js
@@ -1,6 +1,6 @@
 //get
 const getProducts = "SELECT * FROM products";
-const getProductByName = "SELECT * FROM products WHERE name = %$1%"
+const getProductByName = "SELECT * FROM products WHERE name = $1"
 //post
 const addProduct = "INSERT INTO products (name, image, price, store, tag) VALUES ($1, $2, $3, $4, $5)"
 //put
